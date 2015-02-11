aadf7c21009b158687cf1561b496a1c1082e9833
Sarah Joy Calpo & Lee Jorgensen <student@epicodus.com>
2015-02-10 16:25:15 -0800

    make almost everything work

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
-var anagram = function(word, word_list){
-  var word_letters = word.split("").sort();
+var anagram = function(word, word_list) {
+  var word_letters = word.toLowerCase().split("").sort().join("");
   var matching_words_array = [];
-  word_list.forEach(function(possibility) {
-    if (word.split("").sort == possibility.split("").sort) {
-    matching_words_array.push(possibility);
-  }
+  word_list.forEach( function(possibility) {
+    var p_letters = possibility.toLowerCase().split("").sort().join("");
+    if (word_letters === p_letters) {
+      matching_words_array.push(possibility);
+    }
+    else if (p_letters.length < word_letters.length) {
+      var n = 0;
+      while (word_letters.indexOf(p_letters[n]) !== -1 && n <= p_letters.length) {
+        if (n === p_letters.length - 1) {
+          matching_words_array.push(possibility);
+          break;
+        };
+        n++;
+      };
+    }
   });
-  return matching_words_array.join(" ");
+  return matching_words_array.join(", ");
 };
--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,19 @@ describe("anagram", function() {
   it('returns the anagram from a list of two words', function(){
     expect(anagram("ape",["pea","dog"])).to.equal("pea")
   });
+  it('returns two anagrams from a list of three words', function() {
+    expect(anagram("bluest", ["bluets", "bustle", "hustle"])).to.equal("bluets, bustle")
+  });
+  it('will take capitalization into account', function() {
+    expect(anagram("Hops", ["shop", "Posh", "Koss"])).to.equal("shop, Posh")
+  });
+  it('will return partial matches', function() {
+    expect(anagram("path", ["hat", "art"])).to.equal("hat")
+  });
+  it('will deal with duplicate letters in partial matches', function() {
+    expect(anagram("path", ["hat", "art", "haa"])).to.equal("hat")
+  });
+  it('will deal with duplicate letters', function() {
+    expect(anagram("trees", ["reset", "tea"])).to.equal("reset")
+  });
 });
